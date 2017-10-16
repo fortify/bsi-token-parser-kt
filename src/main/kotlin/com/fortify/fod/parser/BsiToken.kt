2690ac41c5ac878f8f899c24fc2b4bf33def88a5
Forrest Peterson <forrest.peterson@hpe.com>
2017-10-16 16:15:15 -0400

    Revert "variables made nullable for klaxon"
    This reverts commit 20d1f57519dee23da5791f2c7f419ee5499ed7f0.

--- src/main/kotlin/com/fortify/fod/parser/BsiToken.kt
+++ src/main/kotlin/com/fortify/fod/parser/BsiToken.kt
@@ -1,25 +1,26 @@
 package com.fortify.fod.parser
 
 class BsiToken {
-    var tenantId: Int? = 0
+    var tenantId: Int = 0
     var tenantCode: String? = null
-    var projectVersionId: Int? = 0
+    var projectVersionId: Int = 0
     var payloadType: String? = null
-    var assessmentTypeId: Int? = 0
+    var assessmentTypeId: Int = 0
     var technologyStack: String? = null
-    var languageLevel: String? = ""
-    var apiUri: String? = "https://api.ams.fortify.com"
-    var scanPreferenceId: Int? = 0
+    var languageLevel = ""
+    var apiUri: String = "https://api.ams.fortify.com"
+    var scanPreferenceId: Int = 0
     var scanPreference: String? = null
-    var includeThirdParty: Boolean? = false
+    var includeThirdParty: Boolean = false
     var auditPreference: String? = "manual"
-    var auditPreferenceId: Int? = 0
-    var includeOpenSourceAnalysis: Boolean? = false
-    var portalUri: String? = "https://ams.fortify.com"
+    var auditPreferenceId: Int = 0
+    var includeOpenSourceAnalysis: Boolean = false
+    var portalUri: String = "https://ams.fortify.com"
 
     var technologyType: String? = null
-    var technologyTypeId: Int? = 0
+    var technologyTypeId: Int = 0
     var technologyVersion: String? = null
-    var technologyVersionId: Int? = 0
+    var technologyVersionId: Int = 0
 
 }
+
