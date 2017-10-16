1ef03fdb5fdbf2b104d6cb9127674fb3e75721e5
Forrest Peterson <forrest.peterson@hpe.com>
2017-10-16 16:10:50 -0400

    first unit test

--- src/test/kotlin/com/fortify/fod/parser/tests/BsiTokenParserSpec.kt
+++ src/test/kotlin/com/fortify/fod/parser/tests/BsiTokenParserSpec.kt
@@ -72,26 +72,28 @@ object BsiTokenParserSpec : Spek({
             }
         }
 
-        var bsiToken1 = "eyJ0ZW5hbnRJZCI6MSwidGVuYW50Q29kZSI6IlRlbmFudDEiLCJyZWxlYXNlSWQiOjYsInBheWxvYWRUeXBlIjoiQU5BTFlTSVNfUEFZTE9BRCIsImFzc2Vzc21lbnRUeXBlSWQiOjgsInRlY2hub2xvZ3lUeXBlIjoiX05FVCIsInRlY2hub2xvZ3lUeXBlSWQiOjEsInRlY2hub2xvZ3lWZXJzaW9uIjoiXzRfMCIsInRlY2hub2xvZ3lWZXJzaW9uSWQiOjUsImF1ZGl0UHJlZmVyZW5jZSI6Ik1hbnVhbCIsImF1ZGl0UHJlZmVyZW5jZUlkIjoxLCJpbmNsdWRlVGhpcmRQYXJ0eSI6ZmFsc2UsImluY2x1ZGVPcGVuU291cmNlQW5hbHlzaXMiOmZhbHNlLCJzY2FuUHJlZmVyZW5jZSI6IlN0YW5kYXJkIiwic2NhblByZWZlcmVuY2VJZCI6MSwicG9ydGFsVXJpIjoiaHR0cDovL2ZvZC5sb2NhbGhvc3QiLCJhcGlVcmkiOiIifQ"
+
+        val bsiToken1 = "eyJ0ZW5hbnRJZCI6MSwidGVuYW50Q29kZSI6IlRlbmFudDEiLCJyZWxlYXNlSWQiOjYsInBheWxvYWRUeXBlIjoiQU5BTFlTSVNfUEFZTE9BRCIsImFzc2Vzc21lbnRUeXBlSWQiOjgsInRlY2hub2xvZ3lUeXBlIjoiX05FVCIsInRlY2hub2xvZ3lUeXBlSWQiOjEsInRlY2hub2xvZ3lWZXJzaW9uIjoiXzRfMCIsInRlY2hub2xvZ3lWZXJzaW9uSWQiOjUsImF1ZGl0UHJlZmVyZW5jZSI6Ik1hbnVhbCIsImF1ZGl0UHJlZmVyZW5jZUlkIjoxLCJpbmNsdWRlVGhpcmRQYXJ0eSI6ZmFsc2UsImluY2x1ZGVPcGVuU291cmNlQW5hbHlzaXMiOmZhbHNlLCJzY2FuUHJlZmVyZW5jZSI6IlN0YW5kYXJkIiwic2NhblByZWZlcmVuY2VJZCI6MSwicG9ydGFsVXJpIjoiaHR0cDovL2ZvZC5sb2NhbGhvc3QiLCJhcGlVcmkiOiIifQ"
 
         on("parsing $bsiToken1") {
-            val token = parser.parse(bisToken1)
+            val token = parser.parse(bsiToken1)
 
             it("should have a Tenant Code of 'Tenant1'") {
                 assertEquals("Tenant1", token.tenantCode)
             }
             it("should have a Project Version ID of '6'") {
-                assertEquals(6, token.projectVersionId)
+               assertEquals(6, token.projectVersionId)
             }
 
             it("should have a payloadType of 'ANALYSIS_PAYLOAD'") {
-                assertEquals("ANALYSIS_PAYLOAD", token.payloadType)
-            }
+               assertEquals("ANALYSIS_PAYLOAD", token.payloadType)
+           }
 
-            it("should have a payloadType of 'portalUri'") {
+           it("should have a payloadType of 'portalUri'") {
                 assertEquals("http://fod.localhost", token.portalUri)
             }
         }
+
 
     }
 })
