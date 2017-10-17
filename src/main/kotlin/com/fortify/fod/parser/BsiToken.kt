283b97ea7c2fee1ca93fd9a00cb6853c57fa12f6
Matt Gibbs <matt@mtgibbs.xyz>
2017-10-17 16:24:35 -0400

    Define better defaults for legacy support (#9)

--- src/main/kotlin/com/fortify/fod/parser/BsiToken.kt
+++ src/main/kotlin/com/fortify/fod/parser/BsiToken.kt
@@ -1,26 +1,38 @@
 package com.fortify.fod.parser
 
 class BsiToken {
+
     var tenantId: Int = 0
-    var tenantCode: String? = null
+    var tenantCode: String = ""
     var projectVersionId: Int = 0
-    var payloadType: String? = null
     var assessmentTypeId: Int = 0
-    var technologyStack: String? = null
-    var languageLevel: String? = ""
-    var apiUri: String = "https://api.ams.fortify.com"
-    var scanPreferenceId: Int = 0
-    var scanPreference: String? = null
+
+    var payloadType: String = "ANALYSIS_PAYLOAD"
+
+    var scanPreferenceId: Int = 1
+    var scanPreference: String = "Standard"
+
+    var auditPreferenceId: Int = 1
+    var auditPreference: String = "Manual"
+
     var includeThirdParty: Boolean = false
-    var auditPreference: String? = "manual"
-    var auditPreferenceId: Int = 0
     var includeOpenSourceAnalysis: Boolean = false
+
     var portalUri: String = "https://ams.fortify.com"
+    var apiUri: String = "https://api.ams.fortify.com"
 
-    var technologyType: String? = null
     var technologyTypeId: Int = 0
+    var technologyType: String? = null
+
     var technologyVersion: String? = null
-    var technologyVersionId: Int = 0
+    var technologyVersionId: Int? = null
 
+    // Legacy aliases
+    var technologyStack: String?
+        get() { return technologyType }
+        set(value) { this.technologyType = value }
+
+    var languageLevel: String?
+        get() { return technologyVersion }
+        set(value) { this.technologyVersion = value }
 }
-
